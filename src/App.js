5ec0a36d4a349775d1da7441c50727ad5799b71f
Juan A. Portela <juanportela@cntravel.es>
2022-12-26 09:40:53 +0100

    adding cardservice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Introduction from './components/Introduction';
 import NavbarMenu from './components/NavbarMenu'
+import Services from './components/Services/Services';
 
 function App() {
   return (
     <div>
       <NavbarMenu></NavbarMenu>
       <Introduction></Introduction>
+      <Services></Services>
     </div>
   );
 }
